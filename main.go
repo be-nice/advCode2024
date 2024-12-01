7142e3d7b5a02976b9204b7d88989489b285d7b7
be-nice <mobcarma@gmail.com>
2024-12-01 12:55:48 +0200

    Added timer for solution benchmarking

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func main() {
+	fmt.Print("Day 1 test input:")
 	day1.Day1(ReadTestInput("day1"))
+
+	fmt.Println("Day 1 solution imput:")
+	start := time.Now()
 	day1.Day1(ReadMainInput("day1"))
+	fmt.Println(time.Since(start))
 }
 
 func ReadTestInput(dir string) []string {
