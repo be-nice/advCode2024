40f3dba86105e3d94f1ebcc9ab640e8c87d4443d
be-nice <mobcarma@gmail.com>
2024-12-02 11:30:53 +0200

    Added day 2

---
+++ day2/day2.go
@@ -0,0 +1,47 @@
+package day2
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func Day2(s []string) {
+	safeCount := 0
+	advSafeCount := 0
+
+	for _, row := range s {
+		nums := strings.Fields(row)
+		if validSeq(nums) {
+			safeCount++
+		} else {
+			for i := range len(nums) {
+				permT := make([]string, len(nums))
+				copy(permT, nums)
+				permT = append(permT[:i], permT[i+1:]...)
+				if validSeq(permT) {
+					advSafeCount++
+					break
+				}
+			}
+		}
+	}
+
+	fmt.Println("Part 1")
+	fmt.Println(safeCount)
+	fmt.Println("Part 2")
+	fmt.Println(safeCount + advSafeCount)
+}
+
+func validSeq(nums []string) bool {
+	diffMap := map[int]int{1: 0, 2: 0, 3: 0}
+	negDiffMap := map[int]int{-1: 0, -2: 0, -3: 0}
+	for i := range len(nums) - 1 {
+		n, _ := strconv.Atoi(nums[i])
+		j, _ := strconv.Atoi(nums[i+1])
+		diffMap[n-j]++
+		negDiffMap[n-j]++
+	}
+
+	return len(diffMap) == 3 || len(negDiffMap) == 3
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"adv/day1"
+	"adv/day2"
 	"bufio"
 	"fmt"
 	"os"
@@ -10,16 +11,26 @@ import (
 )
 
 func main() {
-	fmt.Print("Day 1 test input:")
-	day1.Day1(ReadTestInput("day1"))
+	// fmt.Println("Day 1 test input:")
+	// day1.Day1(ReadTestInputStr("day1"))
 
-	fmt.Println("Day 1 solution imput:")
+	fmt.Println("Day 1 solution:")
 	start := time.Now()
-	day1.Day1(ReadMainInput("day1"))
+	day1.Day1(ReadMainInputStr("day1"))
+	fmt.Println(time.Since(start))
+
+	fmt.Println("----------------------------------------------------")
+
+	// fmt.Println("Day 2 test input:")
+	// day2.Day2(ReadTestInputStr("day2"))
+
+	fmt.Println("Day 2 solution:")
+	start = time.Now()
+	day2.Day2(ReadMainInputStr("day2"))
 	fmt.Println(time.Since(start))
 }
 
-func ReadTestInput(dir string) []string {
+func ReadTestInputStr(dir string) []string {
 	res := []string{}
 
 	file, err := os.Open(filepath.Join(dir, "test.txt"))
@@ -38,7 +49,7 @@ func ReadTestInput(dir string) []string {
 	return res
 }
 
-func ReadMainInput(dir string) []string {
+func ReadMainInputStr(dir string) []string {
 	res := []string{}
 
 	file, err := os.Open(filepath.Join(dir, "main.txt"))
@@ -52,6 +63,44 @@ func ReadMainInput(dir string) []string {
 
 	for scanner.Scan() {
 		res = append(res, scanner.Text())
+	}
+
+	return res
+}
+
+func ReadTestInputByte(dir string) [][]byte {
+	res := [][]byte{}
+
+	file, err := os.Open(filepath.Join(dir, "test.txt"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		res = append(res, scanner.Bytes())
+	}
+
+	return res
+}
+
+func ReadMainInputByte(dir string) [][]byte {
+	res := [][]byte{}
+
+	file, err := os.Open(filepath.Join(dir, "main.txt"))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		res = append(res, scanner.Bytes())
 	}
 
 	return res
